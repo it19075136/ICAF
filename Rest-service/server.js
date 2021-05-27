8d1730d3309ac13b43b73d8eee89242ea9799042
it19075136 <it19075136@my.sliit.lk>
2021-05-28 01:33:12 +0530

    adding initial setup

--- Rest-service/server.js
+++ Rest-service/server.js
@@ -17,24 +17,76 @@ app.use(cors());
 
 /**
  * 
- user routes - {
+ user routes - { - dilmika
     POST, GET - byId, PUT -updateById, DELETE - byId
+
+    model - {
+        name,
+        email,
+        password,
+        gender,
+        type,
+        phoneNumber
+    }
 }
 
-conference routes - {
+conference routes - { - avantha
     POST, GET, PUT, DELETE
+
+    model - {
+        con name,
+        venue,
+        keynoteSpeaker- [ {name,designation,bio} ],
+        startDate,
+        endDate,
+        tracks,
+        description,
+        status,
+        other Details
+    }
+
 }
 
-Submission routes - {
+documents routes - { - pasindu
     POST,GET,PUT, DELETE
+    model - {
+        userId,
+        type ( research paper/ workshop proposals),
+        status,
+        FileUrl,
+        activityId (submission or workshop)
+    }
 }
 
-Workshops routes - {
+submission routes = {
+    POST,GET,PUT, DELETE
+    model - {
+    topic,
+    deadline,
+    description,
+    conferenceId
+    }
+}
+
+Workshops routes - { - thisara
     POST, PUT, GET, DELETE
+    model - {
+        workshop name,
+        workshop description,
+        flyerURL,
+        resource Persons,
+        conferenceId
+    }
 }
 
-Payment routes - {
+Payment routes - { - avantha
     POST, GET
+    model - {
+        userId,
+        type,
+        amount,
+        date
+    }
 }
 
  *  */
