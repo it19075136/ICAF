5d60d40e03ef34c9a8c7e17e5b7f9b867d3cbf0e
it19075136 <it19075136@my.sliit.lk>
2021-05-28 00:41:32 +0530

    adding initial setup

---
+++ Rest-service/dbConfig/key.js
@@ -0,0 +1,3 @@
+const key = "mongodb+srv://rest_user:root1234@cluster0.mebjx.mongodb.net/ICAF?retryWrites=true&w=majority";
+
+module.exports = key
--- Rest-service/server.js
+++ Rest-service/server.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dbKey = require('./dbConfig/key');
+
+mongoose.connect(dbKey, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Connected to database");
+});
 
 let app = express();
 
 app.use(cors());
 
-app.listen(PORT,() => {
-    console.log("Listening on port: ",PORT);
+/**
+ * 
+ user routes - {
+    POST, GET - byId, PUT -updateById, DELETE - byId
+}
+
+conference routes - {
+    POST, GET, PUT, DELETE
+}
+
+Submission routes - {
+    POST,GET,PUT, DELETE
+}
+
+Workshops routes - {
+    POST, PUT, GET, DELETE
+}
+
+Payment routes - {
+    POST, GET
+}
+
+ *  */
+
+
+
+app.listen(PORT, () => {
+    console.log("Listening on port: ", PORT);
 });
