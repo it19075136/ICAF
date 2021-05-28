efddb87d37465660154ca75b7bbb4bd792f740dd
it19020822 <it19020822@my.sliit.lk>
2021-05-28 22:06:18 +0530

    workshop model created

---
+++ Rest-service/models/workshopModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+//-----WorkShop Schema------//
+
+const workshopSchema = new schema({
+    workshopName: {
+        type: String,
+        required: true
+    },
+    workshopDescription: {
+        type: String,
+        required: true
+    },
+    flyerURL: {
+        type: String,
+        required: true
+    },
+    resourcePersons: {
+        type:Array,
+        required:true,
+        personName: {
+            type: String,
+            required: true
+        },
+        designation: {
+            type: String,
+            required: true
+        }
+    },
+    conferenceId: {
+        type: String,
+        required: true
+    }
+
+    
+});
+
+
+module.exports = workShop = mongoose.module('workShop', workshopSchema); 
