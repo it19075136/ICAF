365cd3ad4e217e763f9a82c11f07e8f4544afe67
it19075136 <it19075136@my.sliit.lk>
2021-06-25 17:26:23 +0530

    configured redux

--- webApp/src/app.js
+++ webApp/src/app.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
 import {BrowserRouter, Switch, Route } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers';
 import AddSubmissionForm from './components/submissionManagement/AddSubmissionForm';
 import NavBar from './layouts/navbar';
 import SideDrawer from './layouts/SideDrawer';
 import './app.css';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer,initialState,compose(
+    applyMiddleware(...middleware)
+    // check
+)
+);
 
 export default function app(){
 
     const[sideToggle, setSideToggle] = useState(false);
 
     return (
-        <div>
+        <Provider store={store}>
             <NavBar click={() => setSideToggle(true)}/>
             <SideDrawer show={sideToggle} click={() => setSideToggle(false)}/>
             <BrowserRouter>
@@ -18,6 +32,6 @@ export default function app(){
                 <Route exact path="/submission/upload" component={AddSubmissionForm} />
             </Switch>
             </BrowserRouter>
-        </div>
+        </Provider>
     )
 }
