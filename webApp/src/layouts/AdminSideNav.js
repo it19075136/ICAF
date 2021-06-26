bd17d1bdeb5c8a0575a8c3d8246621a5f31128ab
it19075136 <it19075136@my.sliit.lk>
2021-06-26 14:54:26 +0530

    added admin side navigation

---
+++ webApp/src/layouts/AdminSideNav.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import './AdminSideNav.css'
+
+export default function adminSideNav() {
+
+    return (
+      <div className="sideSection">
+        <Navigation
+          activeItemId={window.location.pathname}
+          onSelect={({ itemId }) => {
+              console.log(itemId)
+            window.location.href = itemId
+          }}
+          items={[
+            {
+              title: "Home",
+              itemId: "/",
+              // Optional
+            //   elemBefore: () => <Icon name="coffee" />
+            },
+            {
+              title: "Submission",
+              itemId: "/submission/add",
+            //   elemBefore: () => <Icon name="user" />,
+              subNav: [
+                {
+                  title: "Add Submission Topic",
+                  itemId: "/submission/add",
+                  // Optional
+                //   elemBefore: () => <Icon name="cloud-snow" />
+                },
+                {
+                    title: "View Submission Topics",
+                    itemId: "/test"
+                }
+              ]
+            },
+            {
+                title: "My Profile",
+                itemId: "/profile"
+            },
+            {
+                title: "Login",
+                itemId: "/signIn"
+            },
+            {
+                title: "Sign Out",
+                itemId: "/sign"
+            }
+          ]}
+        />
+        </div>
+    );
+}
