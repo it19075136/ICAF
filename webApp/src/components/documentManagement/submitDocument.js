98925f3c8f280fb189461f9d7030b3df1510f450
it19075136 <it19075136@my.sliit.lk>
2021-06-27 21:41:20 +0530

    submit document modified

--- webApp/src/components/documentManagement/submitDocument.js
+++ webApp/src/components/documentManagement/submitDocument.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Button} from 'react-bootstrap'
 import DocumentUploader from './documentUploader';
 import './document.css'
+import axios from 'axios'
 
 class submitDocument extends Component {
 
+    state = {
+        userId:"test2u",
+        activityId:"test2av",
+        type:"type2",
+        status:"pending",
+    }
+
+    handleUpload = (e) => {
+
+        e.preventDefault();
+        console.log(this.state);
+
+        const formData = new FormData();
+
+        console.log(this.props.documents[0]);
+
+        formData.append("userId", this.state.userId);
+        formData.append("activityId", this.state.activityId);
+        formData.append("type", this.state.type);
+        formData.append("status", this.state.status);
+        formData.append("file", this.props.documents[0]);
+
+        axios.post('http://localhost:5000/document',formData).then((res) => { // redux action will be added later instead of this
+            console.log("File uploaded!")
+        })
+    }
+
     render() {
-        console.log(this.props.documents)
+
         return (
-            <div className="main">
+            <form className="main" encType="multipart/form-data" onSubmit={this.handleUpload} >
                 <DocumentUploader />
-            </div>
+                <Button type="submit" className="btn btn-primary">Upload</Button>
+            </form>
         )
     }
 }
